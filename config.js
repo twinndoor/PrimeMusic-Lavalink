7dc41ac2c3f4904062557ae4ab36cc3245c41756
twinndoor <cannonbryant197@gmail.com>
2025-03-28 19:32:16 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,16 +3,16 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
+  ownerID: ["754826761673375804", ""], 
   mongodbUri : "mongodb+srv://Dior01:<Bangouttwin5788>@discordbot.naszdsp.mongodb.net/?retryWrites=true&w=majority&appName=Discordbot",
-  spotifyClientId : "",
-  spotifyClientSecret : "",
+  spotifyClientId : "f71a3da30e254962965ca2a89d6f74b9",
+  spotifyClientSecret : "199a619d22dd4e55a4a2c1a7a3d70e63",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "YouTube Music", 
+  activityName: "Join /tris", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "https://discord.gg/wKDfY9hEnP",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
